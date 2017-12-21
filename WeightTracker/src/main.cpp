1694eda645e080cbd430db6caf019240ff6a4483
HMate <mhidvegi@gmail.com>
2017-12-21 14:40:54 +0100

    show weights in a list

--- WeightTracker/src/main.cpp
+++ WeightTracker/src/main.cpp
@@ -1,8 +1,5 @@
 #define _SCL_SECURE_NO_WARNINGS
-#include "common.h"
-#include "logger.h"
-#include "filesystem.h"
-#include "string.h"
+#include <cstring>
 
 #include "imgui/imgui.h"
 #include "imgui/imgui-SFML.h"
@@ -11,8 +8,15 @@
 #include <SFML/System/Clock.hpp>
 #include <SFML/Window/Event.hpp>
 
-void loadWeightFile(const std::string& weightFilePath)
+#include "common.h"
+#include "logger.h"
+#include "filesystem.h"
+#include "string.h"
+
+std::vector<std::string> loadWeightFile(const std::string& weightFilePath)
 {
+    std::vector<std::string> result;
+
     File weightFile;
     weightFile.loadFile(weightFilePath);
     std::string header = weightFile.readLine();
@@ -24,25 +28,27 @@ void loadWeightFile(const std::string& weightFilePath)
         {
             std::string date = parts[0];
             std::string weight = parts[1];
+            result.push_back(line);
         }
         else
             Log::log("Error: less than 2 parts in: %s", line);
     }
+    return result;
 }
 
 int main(char argc, char* argv)
 {
-    sf::RenderWindow window(sf::VideoMode(640, 480), "Weight Tracker");
+    sf::RenderWindow window(sf::VideoMode(1280, 960), "Weight Tracker");
     window.setVerticalSyncEnabled(true);
     ImGui::SFML::Init(window);
 
     sf::Color bgColor;
     float color[3] = {0.0f, 0.0f, 0.0f};
 
-    char weightFilePath[2048] = "weights.dtr";
-    loadWeightFile(weightFilePath);
-
+    char weightFilePath[2048] = "";
     FileSystem::getCurrentWorkingDirectory().copy(weightFilePath, 2048);
+    strcat_s(weightFilePath, "\\weights.dtr");
+    std::vector<std::string> weights = loadWeightFile(weightFilePath);
     
     sf::Clock deltaClock;
     while(window.isOpen())
@@ -79,11 +85,19 @@ int main(char argc, char* argv)
         ImGui::Begin("Tracker");
         {
             ImGui::SetWindowPos(ImVec2(windowPadding, editorHeight + windowPadding));
+            ImGui::SetWindowSize(ImVec2(window.getSize().x - 2.0*windowPadding, window.getSize().y - editorHeight - 2.0f*windowPadding));
             ImGui::InputText("Input file", weightFilePath, 2048);
             if(ImGui::Button("Load file"))
             {
-                loadWeightFile(weightFilePath);
+                weights = loadWeightFile(weightFilePath);
             }
+            ImGui::BeginChild("Sub1", ImVec2(ImGui::GetWindowContentRegionWidth(), 300), false, ImGuiWindowFlags_HorizontalScrollbar);
+            for(auto w : weights)
+            {
+                ImGui::Text(w.c_str());
+            }
+            ImGui::SetScrollHere();
+            ImGui::EndChild();
         }
         ImGui::End();
     
